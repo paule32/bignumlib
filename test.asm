82f82a96484b013dbe7269e751045ded5e03de4d
paule32 <jkallup@web.de>
2018-04-24 22:54:38 +0200

    update

--- test.asm
+++ test.asm
@@ -3,69 +3,40 @@ section .text
 
 global _start
 _start:
-;	push	ebp		; bp - data segment
-;	mov	ebp, esp	; stack frame
-;	sub	esp, 4		; save stack for 1 parameter
+	mov	esi, data_lhs
 
-;	push	data_lhs	; push "data_lhs" string to stack
-	mov	eax, data_lhs
-	push	eax
+	call	strlen
 	call	write_string	; write string
-	pop	eax
-	call	exit_to_cli	; exit program - to console
+	call	exit		; exit program - to console
 
-;	mov	esp, ebp	; get stack
-;	pop	ebp		; restore bp
-	ret			; return to callee function
+	ret			; return to cli
 
-exit_to_cli:
-	mov	eax, 1		; exit()
-	mov	ebx, 0		; return code 0 - normal exit
+exit:
+	mov	al, 1		; exit()
+	mov	bl, 0		; return code 0 - normal exit
 	int	80h
+	ret
 
 strlen:
-;	push	ebp		; bp - data segment
-;	mov	ebp, esp	; stack frame
-;	sub	esp, 4		; save stack for 1 parameter
-
 	; calculate string length
-	mov	esi, eax	; get pointer to string  -"-
-	mov	ecx, -1		; init counter -1
+	mov	edx, 0		; init counter -1
 .strlen01:
-	cmp	byte [esi+ecx], 0 ; is char 0 ?
+	cmp	byte [esi+edx], 0 ; is char 0 ?
 	je	.strlen02	  ; yes, jump to end of function
-	inc	ecx		  ; length + 1
+	inc	edx		  ; length + 1
 	jmp	.strlen01	  ; next character in string
 .strlen02:
-	; place the length of string in eax
-;	sub	edx, ecx	; add + 1 char
-	mov	eax, ecx	; eax = result
-
-;	mov	esp, ebp
-;	pop	ebp		; restore bp
+	add	edx, 1
 	ret			; return to callee function
 
 write_string:
-;	push	ebp		; data segment
-;	mov	ebp, esp	; stack frame
-;	sub	esp, 4		; save stack for 1 parameter
-
-	mov	ecx, data_lhs	; pointer to string
-	mov	edx, 4		; length of string
-	mov	ebx, 1		; stdout
-	mov	eax, 4		; system call (sys_write)
+	mov	ecx, esi	; pointer to string
+	mov	bl, 1		; stdout
+	mov	al, 4		; system call (sys_write)
 	int	80h 		; Linux syscall
-
-;	add	esp, 4		; restore stack
-;	mov	esp, ebp
-;	pop	ebp
 	ret			; return to callee function
 
 section .data
 
 data_lhs:
 	db "241", 10, 0
-
-data_rhs:
-	db "100", 0
-
